e3d7ebd1a4e661a8e867f02e5eafcc5f7f833374
kigpand <kigpand@naver.com>
2022-03-01 21:09:53 +0900

    'wedding-setting'

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
+const AppWrapper = styled.div`
+  background-color: black;
+  width: 100vw;
+  height: 100vh;
+`;
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      테스트
+    </AppWrapper>
   );
 }
 
